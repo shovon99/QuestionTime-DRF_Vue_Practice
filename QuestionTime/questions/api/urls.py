8e449e446ec13097bd610a52588e21fa66470d94
Hasib <hasib99@live.com>
2021-02-10 13:58:43 +0900

    Answer Create and Listing working

--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -8,5 +8,14 @@ router.register(r"questions", qv.QuestionViewSet)
 
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+
+    path("questions/<slug:slug>/answer/",
+            qv.AnswerCreateAPIView.as_view(),
+            name='create-answer'),
+
+    path("questions/<slug:slug>/answers/",
+            qv.AnswerListAPIView.as_view(),
+            name='answer-list'),
+    
 ]
--- QuestionTime/questions/api/views.py
+++ QuestionTime/questions/api/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
-from questions.api.serializers import QuestionSerializer
+from rest_framework import viewsets, generics
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+
+
+from questions.api.serializers import QuestionSerializer, AnswerSerializer
+
 from questions.api.permissions import IsAuthorOrReadOnly
 from rest_framework.permissions import IsAuthenticated
 
 
-from questions.models import Question
+from questions.models import Question, Answer
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
@@ -14,3 +19,27 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes =[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        question = get_object_or_404(Question, slug=kwarg_slug)
+
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this question!")
+        
+        serializer.save(author=request_user, question=question)
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        kwarg_slug = self.kwargs.get("slug")
+        return Answer.objects.filter(question__slug=kwarg_slug).order_by("-created_at")
